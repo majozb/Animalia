805eede8d55a2e011e8e1ced40ec96ee4d2ae6ea
majozb <alu0101516608@ull.edu.es>
2024-11-12 17:03:21 +0000

    fix: some changes in datamodels

--- backend/models/pet.js
+++ backend/models/pet.js
@@ -1,15 +1,36 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 // Define the schema for the Pet collection
 const PetSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'Purchaser', required: true }, // Relatioship with Purchaser collection
+  owner: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    refPath: 'ownerModel', // Reference to either Purchaser or Admin
+    required: true 
+  },
+  ownerModel: { 
+    type: String, 
+    required: true, 
+    enum: ['Purchaser', 'Admin'] // Only allow 'Purchaser' or 'Admin' as values
+  },
+  description: { type: String, required: false },
   type: { type: String, required: true },
-  breed: { type: String, required: false },
+  breed: { type: String, required: true },
   vaccines: { type: Array, required: false, default: [] },
-  age: { type: Number, required: false }
+  birthDate: { type: Date, required: true },
+  medication: { type: Array, required: false, default: [] },
+  images: { 
+    type: Array, 
+    required: true, 
+    validate: {
+      validator: function (v) {
+        return (v.length >= 1 && v.length <= 5); // Ensure the array has at most 5 elements and at least 1
+      },
+      message: props => `${props.path} should have at most 5 image and at least 1.`,
+    },
+  },
 });
 
 const Pet = mongoose.model('Pet', PetSchema);
 
-module.exports = Pet;
+export default Pet;
--- backend/models/purchaser.js
+++ backend/models/purchaser.js
@@ -1,12 +1,27 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 // Define the schema for the Purchaser collection
 const PurchaserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   user: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  email: { type: String, required: false, unique: true },
-  phone: { type: String, required: false },
+  email: { 
+    type: String, 
+    required: false, 
+    validator: function(v) {
+      return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v);
+    },
+  },
+  phone: { 
+    type: String, 
+    required: false,
+    validate: {
+      validator: function(v) {
+        return /^\d{9}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  },
   pets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Pet' }], // Relatioship with Pet collection
   points: { type: Number, required: false, default: 0 },
   purchases : [{ type: mongoose.Schema.Types.ObjectId, ref: 'Purchase' }] // Relatioship with Purchase collection
@@ -14,4 +29,4 @@ const PurchaserSchema = new mongoose.Schema({
 
 const Purchaser = mongoose.model('Purchaser', PurchaserSchema);
 
-module.exports = Purchaser;
+export default Purchaser;
