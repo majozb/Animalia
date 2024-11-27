e0934658cf50c3f9cec2492211ca495c1f879424
adrilgrc <adrianlimagarcia00@gmail.com>
2024-11-27 21:06:09 +0000

    feat: Category Selector Component

--- frontend/src/components/CategorySelector.vue
+++ frontend/src/components/CategorySelector.vue
@@ -1,306 +1,109 @@
 <template>
-  <v-card class="mx-auto" width="300">
-    <v-list v-model:opened="open">
-      <v-list-group v-for="(category, index) in categories" :key="index" :value="category.name">
-        <template v-slot:activator="{ props }">
-          <v-list-item v-bind="props" :title="category.name"></v-list-item>
-        </template>
-
-        <v-list-group v-for="(subcategory, subIndex) in category.subcategories" :key="subIndex" :value="subcategory.name">
+  <v-container>
+    <v-card>
+      <v-list v-model:opened="open">
+        <!-- Categgories -->
+        <v-list-group v-for="(category, index) in categories" :key="`cat-${index}`" :value="`category-${index}`">
           <template v-slot:activator="{ props }">
-            <v-list-item v-bind="props" :title="subcategory.name"></v-list-item>
+            <v-list-item v-bind="props">
+              <!-- The selected-title class is used to underline the selected category -->
+                <v-list-item-title @click.stop="handleSelection(category)" :class="{ 'selected-title': selectedCategory === category }" class="text-truncate">
+                {{ category.name }}
+                </v-list-item-title>
+            </v-list-item>
+
           </template>
 
-          <v-list-item v-for="(subSubcategory, subSubIndex) in subcategory.subcategories" :key="subSubIndex" :title="subSubcategory.name"></v-list-item>
+          <!-- Subcategories -->
+          <v-list-group v-for="(subcategory, subIndex) in category.subcategories" :key="`subcat-${index}-${subIndex}`"
+            :value="`subcategory-${index}-${subIndex}`">
+            <template v-slot:activator="{ props }">
+              <v-list-item v-bind="props">
+                <v-list-item-title @click.stop="handleSelection(subcategory)" :class="{ 'selected-title': selectedCategory === subcategory }">
+                  {{ subcategory.name }}
+                </v-list-item-title>
+              </v-list-item>
+            </template>
+
+            <!-- SubSubcategories -->
+            <v-list-item v-for="(subSubcategory, subSubIndex) in subcategory.subcategories"
+              :key="`subsubcat-${index}-${subIndex}-${subSubIndex}`">
+              <v-list-item-title @click.stop="handleSelection(subSubcategory)" :class="{ 'selected-title': selectedCategory === subSubcategory }">
+                {{ subSubcategory.name }}
+              </v-list-item-title>
+            </v-list-item>
+          </v-list-group>
         </v-list-group>
-      </v-list-group>
-    </v-list>
-  </v-card>
+      </v-list>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
+  props: {
+    categories: {
+      type: Array,
+      required: true,
+    },
+  },
   data() {
     return {
       open: [],
-      categories: [
-        {
-          name: "Alimentación",
-          keywords: ["alimentación"],
-          subcategories: [
-            {
-              name: "Perros",
-              keywords: ["perro", "alimentación"],
-              subcategories: [
-                {
-                  name: "Comida",
-                  keywords: ["perro", "comida", "alimentación"],
-                  subcategories: []
-                },
-                {
-                  name: "Snacks",
-                  keywords: ["perro", "snack", "alimentación"],
-                  subcategories: []
-                },
-              ]
-            },
-            {
-              name: "Gatos",
-              keywords: ["gato", "alimentación"],
-              subcategories: [
-                {
-                  name: "Comida",
-                  keywords: ["gato", "comida", "alimentación"],
-                  subcategories: []
-                },
-                {
-                  name: "Snacks",
-                  keywords: ["gato", "snack", "alimentación"],
-                  subcategories: []
-                },
-              ]
-            },
-            {
-              name: "Aves",
-              keywords: ["ave", "alimentación"],
-              subcategories: [
-                {
-                  name: "Comida",
-                  keywords: ["ave", "comida", "alimentación"],
-                  subcategories: []
-                },
-                {
-                  name: "Snacks",
-                  keywords: ["ave", "snack", "alimentación"],
-                  subcategories: []
-                },
-              ]
-            },
-            {
-              name: "Reptiles",
-              keywords: ["reptil", "alimentación"],
-              subcategories: [
-                {
-                  name: "Comida",
-                  keywords: ["reptil", "comida", "alimentación"],
-                  subcategories: []
-                },
-                {
-                  name: "Snacks",
-                  keywords: ["reptil", "snack", "alimentación"],
-                  subcategories: []
-                },
-              ]
-            },
-            {
-              name: "Peces",
-              keywords: ["pez", "alimentación"],
-              subcategories: [
-                {
-                  name: "Comida",
-                  keywords: ["pez", "comida", "alimentación"],
-                  subcategories: []
-                },
-                {
-                  name: "Snacks",
-                  keywords: ["pez", "snack", "alimentación"],
-                  subcategories: []
-                },
-              ]
-            },
-            {
-              name: "Conejos",
-              keywords: ["conejo", "alimentación"],
-              subcategories: [
-                {
-                  name: "Comida",
-                  keywords: ["conejo", "comida", "alimentación"],
-                  subcategories: []
-                },
-                {
-                  name: "Snacks",
-                  keywords: ["conejo", "snack", "alimentación"],
-                  subcategories: []
-                },
-              ]
-            },
-          ],
-        },
-        {
-          name: "Accesorios",
-          keywords: ["accessorios"],
-          subcategories: [
-            {
-              name: "Perros",
-              keywords: ["perro", "accessorios"],
-              subcategories: [
-                {
-                  name: "Collares",
-                  keywords: ["perro", "collar", "accessorios"],
-                  subcategories: []
-                },
-                {
-                  name: "Correas",
-                  keywords: ["perro", "correa", "accessorios"],
-                  subcategories: []
-                },
-                {
-                  name: "Ropa",
-                  keywords: ["perro", "ropa", "accessorios"],
-                  subcategories: []
-                },
-              ]
-            },
-            {
-              name: "Gatos",
-              keywords: ["gato", "accessorios"],
-              subcategories: [
-                {
-                  name: "Collares",
-                  keywords: ["gato", "collar", "accessorios"],
-                  subcategories: []
-                },
-                {
-                  name: "Correas",
-                  keywords: ["gato", "correa", "accessorios"],
-                  subcategories: []
-                },
-                {
-                  name: "Ropa",
-                  keywords: ["gato", "ropa", "accessorios"],
-                  subcategories: []
-                },
-              ]
-            },
-            {
-              name: "Aves",
-              keywords: ["ave", "accessorios"],
-              subcategories: []
-            },
-            {
-              name: "Reptiles",
-              keywords: ["reptil", "accessorios"],
-              subcategories: [
-                {
-                  name: "Collares",
-                  keywords: ["reptil", "collar", "accessorios"],
-                  subcategories: []
-                },
-                {
-                  name: "Correas",
-                  keywords: ["reptil", "correa", "accessorios"],
-                  subcategories: []
-                },
-              ]
-            },
-            {
-              name: "Peces",
-              keywords: ["pez", "accessorios"],
-              subcategories: []
-            },
-            {
-              name: "Conejos",
-              keywords: ["conejo", "accessorios"],
-              subcategories: []
-            },
-          ],
-        },
-        {
-          name: "Salud e higiene",
-          keywords: ["salud"],
-          subcategories: [
-            {
-              name: "Perros",
-              keywords: ["perro", "salud"],
-              subcategories: [
-                {
-                  name: "Antiparasitarios",
-                  keywords: ["perro", "antiparasitario", "salud"],
-                  subcategories: []
-                },
-                {
-                  name: "Suplementos",
-                  keywords: ["perro", "suplemento", "salud"],
-                  subcategories: []
-                },
-                {
-                  name: "Aseo",
-                  keywords: ["perro", "aseo", "salud"],
-                  subcategories: []
-                },
-              ]
-            },
-            {
-              name: "Gatos",
-              keywords: ["gato", "salud"],
-              subcategories: [
-                {
-                  name: "Antiparasitarios",
-                  keywords: ["gato", "antiparasitario", "salud"],
-                  subcategories: []
-                },
-                {
-                  name: "Suplementos",
-                  keywords: ["gato", "suplemento", "salud"],
-                  subcategories: []
-                },
-                {
-                  name: "Aseo",
-                  keywords: ["gato", "aseo", "salud"],
-                  subcategories: []
-                }
-              ]
-            },
-            {
-              name: "Aves",
-              keywords: ["ave", "salud"],
-              subcategories: [
-                {
-                  name: "Suplementos",
-                  keywords: ["ave", "suplemento", "salud"],
-                  subcategories: []
-                },
-              ]
-            },
-            {
-              name: "Reptiles",
-              keywords: ["reptil", "salud"],
-              subcategories: [
-                {
-                  name: "Suplementos",
-                  keywords: ["reptil", "suplemento", "salud"],
-                  subcategories: []
-                },
-              ]
-            },
-            {
-              name: "Peces",
-              keywords: ["pez", "salud"],
-              subcategories: []
-            },
-            {
-              name: "Conejos",
-              keywords: ["conejo", "salud"],
-              subcategories: [
-                {
-                  name: "Suplementos",
-                  keywords: ["conejo", "suplemento", "salud"],
-                  subcategories: []
-                },
-                {
-                  name: "Aseo",
-                  keywords: ["conejo", "aseo", "salud"],
-                  subcategories: []
-                }
-              ]
-            },
-          ]
-        },
-      ],
+      selectedCategory: null,
     };
+  },
+  methods: {
+    handleSelection(category) {
+      this.selectedCategory = category;
+      this.$emit('updateCategory', category);
+    },
   },
 };
 </script>
 
 <style scoped>
+.v-list-item-title {
+  font-weight: bold;
+  white-space: normal;
+  overflow: visible;
+  text-overflow: unset;
+}
+
+.v-list-item-title:hover {
+  color: #b98142;
+  cursor: pointer;
+}
+
+/*
+  * Add underline to the selected category. Works by using the class 'selected-title' and the
+  :class directive from vue, applying the class if the condition above is true.
+  */
+.selected-title {
+  text-decoration: underline;
+}
+
+@media (max-width: 600px) {
+  .v-list-item-title {
+    font-size: 1em;
+  }
+}
+
+@media (min-width: 601px) and (max-width: 960px) {
+  .v-list-item-title {
+    font-size: 1.1em;
+  }
+}
+
+@media (min-width: 960px) and (max-width: 1280px) {
+  .v-list-item-title {
+    font-size: 1.1em;
+  }
+}
+
+@media (min-width: 1281px) {
+  .v-list-item-title {
+    font-size: 1.2em;
+  }
+}
 </style>
